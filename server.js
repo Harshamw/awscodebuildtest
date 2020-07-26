edaf0e7718fd6a7f5325020f4e52e3c3f4415add
Harsha <hrshweerasinghe@gmail.com>
2020-07-26 21:54:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
