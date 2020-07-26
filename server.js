3d8584a2278bb44e749f61e8dd412aec8e7a33d2
Harsha <hrshweerasinghe@gmail.com>
2020-07-26 21:49:11 +0530

    Port changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5678;
+const port = 3000;
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
